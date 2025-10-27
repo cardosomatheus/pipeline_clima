66e932f21cdad031e10089023804e659db7ed43f
marilan <cardosomcds1@gmail.com>
2025-10-27 15:59:13 -0300

    Aprendendo sobre  @task_group.

--- airflow/dags/temperatura.py
+++ airflow/dags/temperatura.py
@@ -1,4 +1,5 @@
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, task_group
+
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
 
@@ -6,12 +7,6 @@ import requests
 import json 
 from datetime import datetime, timedelta
 
-"""
-Pendencias de melhorias do projeto de estudo:
-    - criar grupo para as tarefas
-    - definir retrys
-    - Definir mais params nas tasks ex: execution_timeout
-"""
 
 @dag(
     schedule='5 * * * *', # a cada hora + 5minutos ex: 16:05
@@ -22,11 +17,9 @@ Pendencias de melhorias do projeto de estudo:
     tags=["Clima", "Temperatura","pipeline", "postgres"]
 )
 def processo_temperatura():
-
     empty_task = EmptyOperator(
         task_id='Empty_task'
     )
-
 
     @task.bash
     def bash_start_pipeline( value_int :int) -> str:
@@ -52,7 +45,8 @@ def processo_temperatura():
                 f'%2FSao_Paulo&start_date={textual_date}&end_date={textual_date}')
 
 
-    @task(execution_timeout=timedelta(seconds=30))
+
+    @task(execution_timeout=timedelta(seconds=30), retries=2)
     def load_hourly_temperatures_today(url :str) -> dict:
         # Faz a requisição da temperatura.
         r = requests.get(url=url)
@@ -63,15 +57,16 @@ def processo_temperatura():
         return dict(r.json())
 
 
-    @task(execution_timeout=timedelta(seconds=30))
+    @task(execution_timeout=timedelta(seconds=30),retries=2)
     def mapping_hourly_temperatures(dict_return :dict) -> json:
         # Mapeia {datahora: temperatura} e salva no json
         list_temperature_to_hours = []
         for i in range(24):
             dict_temperature_to_hours = {}
-            hourly = dict_return['hourly']['time']
-            temperature = dict_return['hourly']['temperature_2m']
-            
+            dict_hourly = dict_return.get('hourly', {})
+            hourly = dict_hourly.get('time',[None])
+            temperature = dict_hourly.get('temperature_2m',[None])
+
             if hourly[i] is None or temperature[i] is None:
                 raise Exception(f'O valor contido na lista está vazio\n Hora:{hourly[i]} temperatura: {temperature[i]}.')
                 
@@ -82,24 +77,25 @@ def processo_temperatura():
         return json.dumps(dict_return)
 
 
-    # Cria a tabela de log junto a sequence
-    create_table_temperatures = SQLExecuteQueryOperator(
-        task_id='create_table_temperature',
-        conn_id='conn_airflow_postgres',
-        autocommit=True,
-        sql='sql/table_temperature.sql'
+    @task_group(group_id = 'postgres')
+    def group_database_postgres():
+        # Cria a tabela de log junto a sequence
+        create_table_temperatures = SQLExecuteQueryOperator(
+            task_id='create_table_temperature',
+            conn_id='conn_airflow_postgres',
+            autocommit=True,
+            sql='sql/table_temperature.sql'
+        )
 
-
-    )
-
-    # Faz o merge da nova requisição.
-    merge_hourly_temperatures = SQLExecuteQueryOperator(
-        task_id='merge_hourly_temperatures',
-        conn_id='conn_airflow_postgres',
-        autocommit=True,
-        sql='sql/merge_temperature.sql'
-    )
-
+        # Faz o merge da nova requisição.
+        merge_hourly_temperatures = SQLExecuteQueryOperator(
+            task_id='merge_hourly_temperatures',
+            conn_id='conn_airflow_postgres',
+            autocommit=True,
+            sql='sql/merge_temperature.sql'
+        )
+        
+        create_table_temperatures >> merge_hourly_temperatures
 
     # Ordenação e execução.
     bash_start     = bash_start_pipeline(value_int=3)
@@ -107,7 +103,7 @@ def processo_temperatura():
     loading_task   = load_hourly_temperatures_today(url_task)
     mapping_task   = mapping_hourly_temperatures(loading_task)
 
-    [empty_task, bash_start, url_task] >> loading_task >> mapping_task >> create_table_temperatures  >> merge_hourly_temperatures
+    [empty_task, bash_start, url_task] >> loading_task >> mapping_task >> group_database_postgres() # create_table_temperatures  >> merge_hourly_temperatures
 
 
 processo_temperatura()
